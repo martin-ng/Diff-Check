20ec398e74899905c4c2429f693b27c2638be263
Martin Ng <martinng1992@gmail.com>
2020-01-31 04:12:50 -0500

    testing travis

--- src/algorithm/lcs.js
+++ src/algorithm/lcs.js
@@ -77,50 +77,17 @@ function findDiff(beforeText, afterText) {
 //   return grid;
 // }
 
-// function lcsTwo(str1, str2) {
-//   var result = [];
-//   for (var i = -1; i < str1.length; i = i + 1) {
-//     result[i] = [];
-//     for (var j = -1; j < str2.length; j = j + 1) {
-//       if (i === -1 || j === -1) {
-//         result[i][j] = 0;
-//       } else if (str1[i] === str2[j]) {
-//         result[i][j] = result[i - 1][j - 1] + 1;
-//       } else {
-//         result[i][j] = Math.max(result[i - 1][j], result[i][j - 1]);
-//       }
-//     }
-//   }
-//   return result;
-// }
+/* 
+1
+*/
 
-// function getLcs(str1, str2, i, j) {
-//   let arr = [];
-//   if (i === 0 || j === 0) {
-//     return "";
-//   } else if (str1[i - 1] === str2[j - 1]) {
-//     return getLcs(str1, str2, i - 1, j - 1) + str1[i - 1];
-//   } else if (arr[(i, j - 1)] > arr[(i - 1, j)]) {
-//     return getLcs(str1, str2, i, j - 1);
-//   } else {
-//     return getLcs(str1, str2, i - 1, j);
-//   }
-// }
+/*
+2
+*/
 
-function getLcs(str1, str2, lcsLengthsMatrix) {
-  var execute = function(i, j) {
-    if (!lcsLengthsMatrix[i][j]) {
-      return "";
-    } else if (str1[i] === str2[j]) {
-      return execute(i - 1, j - 1) + str1[i];
-    } else if (lcsLengthsMatrix[i][j - 1] > lcsLengthsMatrix[i - 1][j]) {
-      return execute(i, j - 1);
-    } else {
-      return execute(i - 1, j);
-    }
-  };
-  return execute(str1.length - 1, str2.length - 1);
-}
+/*
+
+*/
 
 // return function (str1, str2) {
 //   var lcsLengthsMatrix = getLcsLengths(str1, str2);
